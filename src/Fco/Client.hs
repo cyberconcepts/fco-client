73803d54c2e18ea57be8b60543f7a65751005012
Helmut Merz <helmutm@cy55.de>
2018-02-07 11:09:36 +0100

    some restructuring and improvement

--- src/Fco/Client.hs
+++ src/Fco/Client.hs
@@ -8,11 +8,11 @@ module Fco.Client where
 
 import BasicPrelude
 
-import Data.Aeson (decode, encode, object, (.=), Object, Value (Object))
+import Data.Aeson (encode, object, (.=), Object, Value (Object))
 import Data.Aeson.Parser (json)
-import qualified Data.ByteString.Lazy as BS
 import Data.Conduit (($$))
 import Data.Conduit.Attoparsec (sinkParser)
+import Data.Conduit.Combinators (sourceHandle)
 import qualified Data.HashMap.Strict as HM
 import qualified Data.Set as Set
 import Network.HTTP.Client (
@@ -22,6 +22,7 @@ import Network.HTTP.Client (
 import Network.HTTP.Client.Conduit (bodyReaderSource)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import Network.HTTP.Types.Status (statusCode)
+import System.IO (withFile, IOMode (ReadMode))
 import Data.Text (pack)
 
 
@@ -42,18 +43,23 @@ run = do
         putStrLn $
             "Status Code: " ++ (pack $ show (statusCode $ responseStatus resp))
         value <- bodyReaderSource (responseBody resp) $$ sinkParser json
-        print value
+        processValue value
 
 
 loadFromFile :: IO ()
-path = "~/development/data/pocket/output-180122.json"
-loadFromFile = do 
-    input <- BS.readFile path
-    let value = decode (input :: BS.ByteString) :: Maybe Object
+path = "test/data/pocketdata.json"
+loadFromFile =
+    withFile path ReadMode $ \handle -> do
+      value <- sourceHandle handle $$ sinkParser json
+      processValue value
+
+
+processValue :: Value -> IO ()
+processValue value = do
+    --print value
     let v2 = case value of
-          --Just v1 -> HM.keys v1
-          Just v1 -> HM.lookup "list" v1
-          Nothing -> Nothing
+          Object v1 -> HM.lookup "list" v1
+          _ -> Nothing
     let v4 = case v2 of
           Just (Object v3) -> extractTags v3
           Nothing -> Set.fromList ["error"]
@@ -61,9 +67,9 @@ loadFromFile = do
 
 
 extractTags :: HM.HashMap Text Value -> Set.Set Text
-extractTags list = foldl extractAndAdd Set.empty list
-  where extractAndAdd :: Set.Set Text -> Value -> Set.Set Text
-        extractAndAdd set (Object v) = case HM.lookup "tags" v of
+extractTags list = foldr extractAndAdd Set.empty list
+  where extractAndAdd :: Value -> Set.Set Text -> Set.Set Text
+        extractAndAdd (Object v) set = case HM.lookup "tags" v of
           Just (Object x) -> Set.union (Set.fromList (HM.keys x)) set
           Nothing -> Set.fromList ["error"]
   
